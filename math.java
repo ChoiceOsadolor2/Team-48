3b079cfb7bea66a5a1c517baf19c2542c0d2ec1e
Choice Osadolor <240141479@aston.ac.uk>
2025-11-25 17:06:49 +0000

    Shop all page created, displays products, filter for categories and generic header

--- math.java
+++ math.java
@@ -1,11 +1,45 @@
-public class math{
-    public Math(){}
+public class Math {
 
-    public int max(int num; int num2){
-        if( num > num2){
-            return num;
-        }else{
+    // Constructor (optional, but corrected to match class name)
+    public Math() {}
+
+    public int max(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
             return num2;
-        }    
+        }
+    }
+
+    public int min(int num1, int num2) {
+        if (num1 < num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multi(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int mod(int num1, int num2) {
+        return num1 % num2;
+    }
+
+    public double pow(int num1, int num2) {
+        return Math.pow(num1, num2);
     }
 }
