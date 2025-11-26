30f7e14161cf5965c40cc00f2e011e034aba0641
Choice Osadolor <240141479@aston.ac.uk>
2025-11-26 18:37:03 +0000

    js added to animate the filter(onscroll->show)

--- scripts/animations.js
+++ scripts/animations.js
@@ -28,10 +28,12 @@ if(section){
 }
 if(section_main){
     section_main.style.width='30em';
-
 }
 
 if(filter){
+    document.getElementById('toggle_down').style.display='flex';
+        document.getElementById('toggle_up').style.display='none';
+
     filter_options.style.height='70%';
 }
 }
@@ -39,6 +41,31 @@ if(filter){
 function closeSection(id){
     const section = document.getElementById(id);
     const section_main = document.getElementById(`${id}_main`);
+    const filter_options= document.getElementById(`${id}_content`);
+
     if(section_main) section_main.style.width='0';
     if(section) section.style.display='none';
-}
+
+if(filter_options){
+    document.getElementById('toggle_down').style.display='none';
+        document.getElementById('toggle_up').style.display='flex';
+
+    filter_options.style.height='0%';
+    section.style.display='flex'
+}}
+
+
+// Show filter, Hide Filter on scroll
+
+const filter = document.getElementById("filter");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+        filter.style.opacity = "0";
+    } else {
+        filter.style.opacity = "1";
+    }
+
+    lastScrollY = window.scrollY;
+});
