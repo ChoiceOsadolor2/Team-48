023787d626ad7e50b5ab8a567cdaeae1d394b435
Gurneet Aujla <gugz05.2@gmail.com>
2025-12-02 20:20:43 +0000

    Fix CI failure by updating ProductSeeder with required fields

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,14 +13,18 @@ class ProductSeeder extends Seeder
         $category = Category::create([
             'name' => 'PC Games',
             'slug' => 'pc-games',
+            'description' => 'PC gaming category',
         ]);
 
         Product::create([
             'category_id' => $category->id,
             'name' => 'Elden Ring',
             'slug' => 'elden-ring',
+            'description' => 'Fantasy action RPG',
             'price' => 49.99,
             'stock' => 10,
+            'platform' => 'PC',
+            'image_url' => 'https://example.com/elden-ring.jpg',
         ]);
     }
 }
