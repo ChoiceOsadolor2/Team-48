d7e867b43ff237229e509232cb846a338280972b
Choice Osadolor <240141479@aston.ac.uk>
2026-02-06 01:34:33 +0000

    Light and Dark Mode Toggle Fix 1/2-- Th toggle is uses local storage and saves themes

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -81,17 +81,98 @@ window.addEventListener("scroll", () => {
 
 
 
-// Dark Mode Toggle 
-function myFunction() {
-   var element = document.body;
-   const wrapper_overlay = document.getElementById("wrapper_overlay");
-   const header = document.querySelectorAll("header");
+
+                                            // Dark Mode Toggle 
+document.querySelector("html").setAttribute("data-theme", 'light');
 
 
-   element.classList.toggle("light-mode");
-   wrapper_overlay.style.backgroundImage = element.classList.contains("light-mode") ? "url('../assets/image.png')" : "url('../assets/ShopAll_BG.png')";
-   header.style.color='black';
-    header.style.backgroundColor = element.classList.contains("light-mode") ? 'rgba(5,0,54, 0.4)' : 'rgba(5, 0, 54, 0.4)';
-    header.style.backdropFilter = element.classList.contains("light-mode") ? 'blur(20px)' : 'none'; 
-    header.style.backgroundColor='rbga(5, 0, 54, 0.4)';
+// Create a button element for theme toggling
+const ThemeToggleContainer = document.createElement('div');
+ThemeToggleContainer.setAttribute('id', 'theme-toggle-container');
+document.body.appendChild(ThemeToggleContainer);
+
+const ThemeToggle= document.createElement('input');
+const ThemeToggleLabel = document.createElement('label');
+ThemeToggleLabel.setAttribute('for', 'theme-toggle-button');
+ThemeToggle.type = "checkbox";
+ThemeToggle.id = "theme-toggle-button";
+ThemeToggle.setAttribute('data-theme-toggle', '');
+ThemeToggle.setAttribute('aria-label', 'Change to light theme');
+
+
+ThemeToggleContainer.appendChild(ThemeToggle);  
+ThemeToggleContainer.appendChild(ThemeToggleLabel);         
+
+
+// Sourced from Codepen
+/**
+* Utility function to calculate the current theme setting.
+* Look for a local storage value.
+* Fall back to system setting.
+* Fall back to light mode.
+*/
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
 }
+
+/**
+* Utility function to update the button text and aria-label.
+*/
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.innerText = newCta;
+}
+
+/**
+* Utility function to update the theme setting on the html tag
+*/
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+/**
+* On page load:
+*/
+
+/**
+* 1. Grab what we need from the DOM and system settings on page load
+*/
+const button = ThemeToggle;
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+* 2. Work out the current site settings
+*/
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+/**
+* 3. Update the theme setting and button text accoridng to current settings
+*/
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+* 4. Add an event listener to toggle the theme
+*/
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+}); 
