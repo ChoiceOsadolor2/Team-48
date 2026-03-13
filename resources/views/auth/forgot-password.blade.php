7ff0a2006c4ee6e3acc17630f433f39ac8e372fb
Filip-2002 <240080064@aston.ac.uk>
2026-03-13 23:38:42 +0000

    Made tab title and favicon the same across all pages

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Forgot Password</title>
+    <title>Veltrix</title>
+    <link rel="icon" href="/assets/MainLogo.png" type="image/png">
 
     <link rel="stylesheet" href="/styles/Login,SIgn-up.css">
     <link rel="stylesheet" href="/styles/style.css">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Veltrix</title>
+        <link rel="icon" href="/assets/MainLogo.png" type="image/png">
         <script>
             (function () {
                 try {
